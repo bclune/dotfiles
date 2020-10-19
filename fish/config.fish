164694681d94360d79c19a1e5b07e942549acd9a
Brendan Clune <brendan.clune@stateless.net>
2020-10-19 12:40:26 -0600

    Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+set fish_greeting
+set DXVK_CONFIG_FILE $HOME/.config/dxvk.conf
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t ""
+            commandline -f history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
